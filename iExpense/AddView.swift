a71ee7c71d99afc051295a980d6f1827ad836340
Faisal-Herawy <faisalherawy@icloud.com>
2024-01-02 18:34:45 +0300

    finished project 7, seems fun

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.Swift
+//  iExpense
+//
+//  Created by Faisal Herawy on 20/06/1445 AH.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
