7f0904df05a89beec068839b6111412ee97e963e
Faisal-Herawy <faisalherawy@icloud.com>
2024-01-02 13:37:38 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Faisal Herawy on 20/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
